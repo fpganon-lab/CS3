945381d5e5ade58820301528b6183814f4a62a28
fpganon-lab <fpganon@zrc.pshs.edu.ph>
2026-09-11 14:35:46 +0800

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        return
+
+    def take_damage(self, amount):
+        self.hp -= amount
+        return
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+arthur.take_damage(10)
+print(f"{arthur.name}'s HP: {arthur.hp}")
+print(f"{morgana.name}'s HP: {morgana.hp}")
